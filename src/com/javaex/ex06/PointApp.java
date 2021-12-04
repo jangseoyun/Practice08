0651d6acaff1df03fb32013b657e3e130cb1c758
장서윤 <may-primavera@naver.com>
2021-12-04 21:19:34 +0900

    Practice08 /Object Class

--- src/com/javaex/ex06/PointApp.java
+++ src/com/javaex/ex06/PointApp.java
@@ -9,12 +9,12 @@ public class PointApp {
 		Point p3 = new Point(5, 3);
 		Point p4 = p1;
 		
-		System.out.println(p1 == p2);
-		System.out.println(p2 == p3);
-		System.out.println(p3 == p4);
-		System.out.println(p4 == p1);		
-		System.out.println(p1.equals(p2));  
-		System.out.println(p4.equals(p1));
+		System.out.println(p1 == p2); //false
+		System.out.println(p2 == p3); //false
+		System.out.println(p3 == p4); //false
+		System.out.println(p4 == p1); //true		
+		System.out.println(p1.equals(p2)); //false
+		System.out.println(p4.equals(p1)); //true
 		//모든 클래스는 Object를 상속 관계에 있음. Object안에 있는 것을 가져다가 쓰는 것
 	}
 
