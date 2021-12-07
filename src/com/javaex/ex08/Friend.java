a6fc3ab637ca2288fda376cf1a60ade5fd539d61
장서윤 <may-primavera@naver.com>
2021-12-07 23:47:26 +0900

    Practice08 과제

--- src/com/javaex/ex08/Friend.java
+++ src/com/javaex/ex08/Friend.java
@@ -1,5 +1,53 @@
 package com.javaex.ex08;
 
 public class Friend {
+	// 필드
+	private String name, ph, school;
+
+	// 생성자
+	public Friend() {
+	}
+
+	public Friend(String name, String ph, String school) {
+		super();
+		this.name = name;
+		this.ph = ph;
+		this.school = school;
+	}
+
+	// 메소드g,s
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPh() {
+		return ph;
+	}
+
+	public void setPh(String ph) {
+		this.ph = ph;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	// 메소드 일반
+	@Override
+	public String toString() {
+		return "Friend [name=" + name + ", ph=" + ph + ", school=" + school + "]";
+	}
+
+	public void draw() {
+		System.out.println("이름:" + name + " 핸드폰:" + ph + " 학교:" + school);
+	}
 
 }
--- src/com/javaex/ex08/FriendApp.java
+++ src/com/javaex/ex08/FriendApp.java
@@ -1,10 +1,29 @@
 package com.javaex.ex08;
 
+import java.util.Scanner;
+
 public class FriendApp {
 
 	public static void main(String[] args) {
-		
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("친구를 3명 등록해 주세요");
+		Friend friend = new Friend();
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.print(">>");
+			String people = sc.nextLine();
+
+			String[] sArray = people.split(" ");
+
+			friend.setName(sArray[0]);
+			friend.setPh(sArray[1]);
+			friend.setSchool(sArray[2]);
+		}
+
+
+		sc.close();
 	}
 
 }
