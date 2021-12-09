8a5188fc3f2ada155279f1d65e8a39e1046b5944
장서윤 <may-primavera@naver.com>
2021-12-09 17:49:15 +0900

    Practice08 Wrapper class

--- src/com/javaex/ex08/FriendApp.java
+++ src/com/javaex/ex08/FriendApp.java
@@ -1,29 +1,58 @@
 package com.javaex.ex08;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FriendApp {
 
 	public static void main(String[] args) {
 
+		System.out.println("=====리스트 사용================");
 		Scanner sc = new Scanner(System.in);
+		List<Friend> fList = new ArrayList<Friend>();
 
 		System.out.println("친구를 3명 등록해 주세요");
-		Friend friend = new Friend();
 
-		for (int i = 1; i <= 3; i++) {
-			System.out.print(">>");
-			String people = sc.nextLine();
+		for (int i = 0; i < 3; i++) {
+			String f = sc.nextLine();
+			String[] fobj = f.split(" ");
 
-			String[] sArray = people.split(" ");
-
-			friend.setName(sArray[0]);
-			friend.setPh(sArray[1]);
-			friend.setSchool(sArray[2]);
+			Friend friend = new Friend(fobj[0], fobj[1], fobj[2]);
+			fList.add(friend);
 		}
 
+		for (Friend f : fList) {
+			f.draw();
+		}
+
+		System.out.println("===배열 사용==================");
+
+		Friend[] friendArray = new Friend[3];
+		
+		Scanner sc02 = new Scanner(System.in);
+
+		System.out.println("친구를 3명 등록해 주세요");
+
+		for (int i = 0; i < 3; i++) {
+			String f = sc.nextLine();
+			String[] fobj = f.split(" ");
+
+			// 한번 정의해서 계속 사용할 수 있도록
+			String name = fobj[0];
+			String ph = fobj[1];
+			String school = fobj[2];
+
+			Friend friend = new Friend(name, ph, school);
+			friendArray[i] = friend;
+		}
+
+		for (int i = 0; i<friendArray.length; i++) {
+			friendArray[i].draw();
+		}
 
 		sc.close();
+
 	}
 
 }
